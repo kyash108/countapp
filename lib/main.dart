6e756c9e6d38e304bf40dfb0184e884555288587
Yash Kumar <w0749495@myscc.ca>
2023-02-27 17:05:32 -0500

    added futurebuilder to the app

--- lib/main.dart
+++ lib/main.dart
@@ -34,8 +34,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  var total = 0;
-  var left = 0;
+  var total;
+  var left;
 
   Future<void> totalsCount() async {
     final response = await http.get(Uri.parse('https://ykumar.scweb.ca/countit/readCount.php'));
@@ -115,8 +115,12 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
-    totalsCount();
-    leftsCount();
+    total = 0;
+    left = 0;
+    setState(() {
+      totalsCount();
+      leftsCount();
+    });
   }
 
 
@@ -142,195 +146,205 @@ class _MyHomePageState extends State<MyHomePage> {
           },
         ),
       ),
-      body: SingleChildScrollView(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          child: Center(
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 10.0),
-                  child: Text("How many times do you have to read?",style: TextStyle(color: Colors.white, fontSize: 20),),
-                ),
-                Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Stack(
+      body: FutureBuilder(
+        future: totalsCount(), // replace with your actual asynchronous function
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return SingleChildScrollView(
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  child: Center(
+                    child: Column(
                       children: [
-                        Container(
-                          decoration: BoxDecoration(
-                            color: Color(0xff53a99a),
-                            shape: BoxShape.circle,
-                            boxShadow: [
-                              BoxShadow(
-                                offset: Offset(0, 27),
-                                blurRadius: 33,
-                                color: Color(0x3827ae96),
-                              )
-                            ],
-                          ),
-                          height: 200.0,
+                        Padding(
+                          padding: const EdgeInsets.only(top: 10.0),
+                          child: Text("How many times do you have to read?",style: TextStyle(color: Colors.white, fontSize: 20),),
                         ),
-                        Container(
-                            margin: EdgeInsets.fromLTRB(0, 50, 0, 0),
-                            alignment: Alignment.center,
-                            child: Text("$total",
-                              style: TextStyle(
-                                fontFamily: 'Helvetica',
-                                fontWeight: FontWeight.bold,
-                                fontSize: 120,
-                                color: Colors.white,
+                        Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Stack(
+                              children: [
+                                Container(
+                                  decoration: BoxDecoration(
+                                    color: Color(0xff53a99a),
+                                    shape: BoxShape.circle,
+                                    boxShadow: [
+                                      BoxShadow(
+                                        offset: Offset(0, 27),
+                                        blurRadius: 33,
+                                        color: Color(0x3827ae96),
+                                      )
+                                    ],
+                                  ),
+                                  height: 200.0,
+                                ),
+                                Container(
+                                    margin: EdgeInsets.fromLTRB(0, 50, 0, 0),
+                                    alignment: Alignment.center,
+                                    child: Text("$total",
+                                      style: TextStyle(
+                                        fontFamily: 'Helvetica',
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 120,
+                                        color: Colors.white,
+                                      ),
+                                    )
+                                ),
+                              ],
+                            )
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: TextButton(
+                                style: ButtonStyle(
+                                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                        RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.circular(18.0),
+                                            side: BorderSide(color: Colors.white)
+                                        )
+                                    )
+                                ),
+                                onPressed: (){
+                                  setState(()  {
+                                    updateCounts();
+                                    totalsCount();
+                                    leftsCount();
+                                    total += 1;;
+                                  });
+                                },
+                                child: Text("+",style: TextStyle(color: Colors.red,fontSize: 50),),
                               ),
-                            )
-                        ),
-                      ],
-                    )
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: TextButton(
-                        style: ButtonStyle(
-                            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                                RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(18.0),
-                                    side: BorderSide(color: Colors.white)
-                                )
-                            )
-                        ),
-                        onPressed: (){
-                          setState(()  {
-                            updateCounts();
-                            totalsCount();
-                            leftsCount();
-                            total += 1;;
-                          });
-                        },
-                        child: Text("+",style: TextStyle(color: Colors.red,fontSize: 50),),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: TextButton(
-                        style: ButtonStyle(
-                            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                                RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(18.0),
-                                    side: BorderSide(color: Colors.white)
-                                )
-                            )
-                        ),
-                        onPressed: (){
-                          setState(() {
-                            if(total == 0){
-                              total = 0;
-                            }else{
-                              total -= 1;
-                              decreaseTotal();
-                            }
-                          });
-                        },
-                        child: Text("-",style: const TextStyle(color: Colors.red,fontSize: 50),),
-                      ),
-                    ),
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 10.0),
-                  child: Text("Times Completed?",style: TextStyle(color: Colors.white, fontSize: 20),),
-                ),
-                Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Stack(
-                      children: [
-                        Container(
-                          decoration: BoxDecoration(
-                            color: Color(0xff53a99a),
-                            shape: BoxShape.circle,
-                            boxShadow: [
-                              BoxShadow(
-                                offset: Offset(0, 27),
-                                blurRadius: 33,
-                                color: Color(0x3827ae96),
-                              )
-                            ],
-                          ),
-                          height: 200.0,
-                        ),
-                        Container(
-                            margin: EdgeInsets.fromLTRB(0, 50, 0, 0),
-                            alignment: Alignment.center,
-                            child: Text("$left",
-                              style: TextStyle(
-                                fontFamily: 'Helvetica',
-                                fontWeight: FontWeight.bold,
-                                fontSize: 120,
-                                color: Colors.white,
-                              ),
-                            )
-                        ),
-                      ],
-                    )
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: TextButton(
-                        style: ButtonStyle(
-                          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(18.0),
-                                side: BorderSide(color: Colors.white)
                             ),
-                          ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: TextButton(
+                                style: ButtonStyle(
+                                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                        RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.circular(18.0),
+                                            side: BorderSide(color: Colors.white)
+                                        )
+                                    )
+                                ),
+                                onPressed: (){
+                                  setState(() {
+                                    if(total == 0){
+                                      total = 0;
+                                    }else{
+                                      total -= 1;
+                                      decreaseTotal();
+                                    }
+                                  });
+                                },
+                                child: Text("-",style: const TextStyle(color: Colors.red,fontSize: 50),),
+                              ),
+                            ),
+                          ],
                         ),
-                        onPressed: (){
-                          setState(() {
-                            if(total>=1){
-                              updateCountsLefts();
-                              leftsCount();
-                              decreaseTotal();
-                              left +=1;
-                              total -=1;
-                            }
-                          });
-                        },
-                        child: Text("+",style: TextStyle(color: Colors.red,fontSize: 50),),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: TextButton(
-                        style: ButtonStyle(
-                            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                                RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(18.0),
-                                    side: BorderSide(color: Colors.white)
-                                )
+                        Padding(
+                          padding: const EdgeInsets.only(top: 10.0),
+                          child: Text("Times Completed?",style: TextStyle(color: Colors.white, fontSize: 20),),
+                        ),
+                        Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Stack(
+                              children: [
+                                Container(
+                                  decoration: BoxDecoration(
+                                    color: Color(0xff53a99a),
+                                    shape: BoxShape.circle,
+                                    boxShadow: [
+                                      BoxShadow(
+                                        offset: Offset(0, 27),
+                                        blurRadius: 33,
+                                        color: Color(0x3827ae96),
+                                      )
+                                    ],
+                                  ),
+                                  height: 200.0,
+                                ),
+                                Container(
+                                    margin: EdgeInsets.fromLTRB(0, 50, 0, 0),
+                                    alignment: Alignment.center,
+                                    child: Text("$left",
+                                      style: TextStyle(
+                                        fontFamily: 'Helvetica',
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 120,
+                                        color: Colors.white,
+                                      ),
+                                    )
+                                ),
+                              ],
                             )
                         ),
-                        onPressed: (){
-                          setState(() {
-                            if(left == 0){
-                              left = 0;
-                            }else{
-                              left -=1;
-                              decreaseLeft();
-                            }
-                          });
-                        },
-                        child: Text("-",style: const TextStyle(color: Colors.red,fontSize: 50),),
-                      ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: TextButton(
+                                style: ButtonStyle(
+                                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(18.0),
+                                        side: BorderSide(color: Colors.white)
+                                    ),
+                                  ),
+                                ),
+                                onPressed: (){
+                                  setState(() {
+                                    if(total>=1){
+                                      updateCountsLefts();
+                                      leftsCount();
+                                      decreaseTotal();
+                                      left +=1;
+                                      total -=1;
+                                    }
+                                  });
+                                },
+                                child: Text("+",style: TextStyle(color: Colors.red,fontSize: 50),),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: TextButton(
+                                style: ButtonStyle(
+                                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                                        RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.circular(18.0),
+                                            side: BorderSide(color: Colors.white)
+                                        )
+                                    )
+                                ),
+                                onPressed: (){
+                                  setState(() {
+                                    if(left == 0){
+                                      left = 0;
+                                    }else{
+                                      left -=1;
+                                      decreaseLeft();
+                                    }
+                                  });
+                                },
+                                child: Text("-",style: const TextStyle(color: Colors.red,fontSize: 50),),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
-                  ],
+                  ),
                 ),
-              ],
-            ),
-          ),
-        ),
+              );
+          } else {
+            // return a loading indicator or some other temporary widget
+            return Center(child: CircularProgressIndicator());
+          }
+        },
       ),
     );
   }
